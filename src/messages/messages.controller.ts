799a0dd472325fd1d6b5c1d9a841dc3a9b0e7451
Akhilesh yadav <akhileshyadav310596@gmail.com>
2024-02-08 20:40:00 +0530

    update

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,20 +1,25 @@
 import { Controller, Get, Post, Body, Param } from '@nestjs/common';
-
+import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
+   messagesService: MessagesService;
+   constructor(){
+      this.messagesService = new MessagesService();
+   }
    @Get()
    listMessages() {
-
+      return this.messagesService.findAll();
    }
    
    @Post()
-   craeteMessage(){
-
+   createMessage(@Body() body:CreateMessageDto){
+     return this.messagesService.create(body.content);
    }
 
    @Get('/:id')
-   getMessage(){
-
+   getMessage(@Param('id') id: string){
+     return this.messagesService.findOne(id);
    }
 }
